aeba7d6080b9f4fcffd4bad7d4326b0001c538d7
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2025-11-13 16:45:16 +0300

    Set up navigation between pages with Header component

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-5xl font-light">Users Page</h1>
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="flex-grow flex justify-center items-center bg-gray-50">
+        <h1 className="text-5xl font-light">Users Page</h1>
+      </main>
     </div>
   );
 };
